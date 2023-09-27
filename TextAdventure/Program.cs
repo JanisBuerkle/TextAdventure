a628ffaf95f8b6953f02630d07219d8aba658df2
Janis Bürkle <bks@teamtechnik.com>
2023-09-27 15:25:46 +0200

    Super

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -223,7 +223,7 @@ class Program
                 break;
         }
     }
-    //Gut bessa
+    //Gut 
     public void Wildschwein()
     {
         string formatierteMonsterhp = variable.Monsterhp.ToString("F0");
